7d4ff2bb66030918d46cad38745e4766e7c05842
Milena <milenis.2102@gmail.com>
2025-01-25 11:16:43 -0500

    Se crea micro servicio de payments

---
+++ payments/src/test/java/ms/books/payments/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.books.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
