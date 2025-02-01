7f74adbdf4fa5d433f01625aaff187c1a679ba1c
Milena <milenis.2102@gmail.com>
2025-01-31 21:58:40 -0500

    Implementar campos de Orders

--- payments/src/main/java/ms/books/payments/controller/model/CreateOrderedRequest.java
+++ payments/src/main/java/ms/books/payments/controller/model/CreateOrderedRequest.java
@@ -1,18 +1,14 @@
 package ms.books.payments.controller.model;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import org.springframework.format.annotation.NumberFormat;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 public class CreateOrderedRequest {
 
     @NotNull(message = "totalAmount cannot be null")
     private BigDecimal totalAmount;
 
-    
+
 
 }
